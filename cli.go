19c558fafc39f6e3faaee39c7ecf49bb56fd7c4c
Ondrej Zavodsky <zavodsky.ondrej1@gmail.com>
2026-07-23 06:58:14 +0200

    added the cli flags for control:

---
+++ cli.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"flag"
+	"strings"
+)
+
+type Config struct {
+	Tags       []string
+	MatchCount int
+}
+
+func ParseFlags() Config {
+	tagsFlag := flag.String("tags", "", "comma-separated tags, e.g. tag1,tag2")
+	fullMatch := flag.Bool("f", false, "require full match")
+	matchCount := flag.Int("m", 1, "number of matching entries")
+
+	flag.Parse()
+
+	var tags []string
+	if *tagsFlag != "" {
+		tags = strings.Split(*tagsFlag, ",")
+	}
+	if *fullMatch {
+		return Config{Tags: tags,
+			MatchCount: len(tags)}
+	}
+	return Config{
+		Tags:       tags,
+		MatchCount: *matchCount,
+	}
+}
